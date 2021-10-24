30a13cbf8b15f5107540a91fb44cfd4eddd96efb
tonouchi510 <tonouchi27@gmail.com>
2021-10-24 15:27:33 +0900

    Update utils

--- utils/__init__.py
+++ utils/__init__.py
@@ -5,7 +5,14 @@ Trainingの汎用化したパッケージ.
 """
 
 from .logger import *
+from .data import *
+from .trainer import *
 
 __all__ = (
     "get_logger",
+    "preprocess_image",
+    "TFRECORD_FEATURES",
+    "get_labels",
+    "get_tfrecord_dataset",
+    "Training"
 )
