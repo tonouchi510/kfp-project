e4866f1eb2e5a6fe4b3bda5377a9b13dc742457f
tonouchi510 <tonouchi27@gmail.com>
2022-02-14 18:15:09 +0900

    Add head-pose-dataset-pipeline

---
+++ pipelines/head-pose-dataset-pipeline/data-chunk-spliter/main.py
@@ -0,0 +1,67 @@
+import json
+import hashlib
+from absl import app
+from absl import flags
+from google.cloud import storage
+
+FLAGS = flags.FLAGS
+flags.DEFINE_string(
+    "pipeline", None,
+    "Name of pipeline")
+
+flags.DEFINE_string(
+    "job_id", "test",
+    "ID for job management.")
+
+flags.DEFINE_string(
+    "bucket_name", "",
+    "GCS bucket name")
+
+flags.DEFINE_string(
+    "dataset", None,
+    "Directory where dataset is stored.")
+
+N_FILES_BY_TFRECORD = 1000
+
+
+def main(argv):
+    if len(argv) > 1:
+        raise app.UsageError('Too many command-line arguments.')
+
+    job_dir = f"tmp/{FLAGS.pipeline}/{FLAGS.job_id}/data-chunk-spliter"
+
+    client = storage.Client()
+    bucket = client.bucket(FLAGS.bucket_name)
+
+    prefix = FLAGS.dataset.replace(f"gs://{FLAGS.bucket_name}/", "")
+    blobs = bucket.list_blobs(prefix=prefix)
+    files = []
+    for b in blobs:
+        files.append(b.name)
+    files.remove(f"{prefix}/")  # フォルダ自身を削除
+    n_files = len(files)
+
+    i = 0
+    chunk_files = []
+    while i < n_files:
+        targets = []
+        j = 0
+        while (i + j) < n_files and j < N_FILES_BY_TFRECORD:
+            targets.append(files[i + j])
+            j += 1
+        f_number: str = hashlib.md5(str(targets[0:5]).encode()).hexdigest()
+        chunk_file = f"chunk_{f_number}.txt"
+        with open(chunk_file, "w") as f:
+            for id in targets:
+                f.write("%s\n" % id)
+        blob = bucket.blob(f"{job_dir}/{chunk_file}")
+        blob.upload_from_filename(chunk_file)
+        chunk_files.append(f"{job_dir}/{chunk_file}")
+        i += j
+
+    with open("/tmp/out.json", "w") as f:
+        json.dump(chunk_files, f, indent=4)
+
+
+if __name__ == '__main__':
+    app.run(main)
---
+++ pipelines/head-pose-dataset-pipeline/head-pose-dataset-pipeline.py
@@ -0,0 +1,54 @@
+import kfp
+from kfp import dsl
+from kfp import gcp
+
+# Initialize component store
+component_store = kfp.components.ComponentStore(
+    local_search_paths=["pipelines/head-pose-dataset-pipeline", "components"])
+
+# Create component factories
+converter_op = component_store.load_component("converter")
+data_chunk_spliter_op = component_store.load_component("data-chunk-spliter")
+slack_notification_op = component_store.load_component("slack-notification")
+
+
+# Define pipeline
+@dsl.pipeline(
+    name="head-pose-dataset pipeline",
+    description="Create tfrecord dataset for head-pose-pipeline."
+)
+def pipeline(
+    pipeline_name: str = "head-pose-dataset-pipeline",
+    bucket_name: str = "",
+    job_id: str = "{{JOB_ID}}",
+    dataset: str = "",
+    valid_ratio: float = 0.1,
+):
+
+    with dsl.ExitHandler(exit_op=slack_notification_op(
+        pipeline=pipeline_name,
+        bucket=bucket_name,
+        jobid=job_id,
+        message="Status: {{workflow.status}}")
+    ):
+        split_task = data_chunk_spliter_op(
+            pipeline=pipeline_name,
+            bucket_name=bucket_name,
+            job_id=job_id,
+            dataset=dataset,
+        ).apply(gcp.use_preemptible_nodepool()) \
+            .set_retry(num_retries=2)
+
+        with dsl.ParallelFor(split_task.output) as item:
+            converter_op(
+                pipeline=pipeline_name,
+                bucket=bucket_name,
+                job_id=job_id,
+                valid_ratio=valid_ratio,
+                chunk_file=item
+            ).apply(gcp.use_preemptible_nodepool()) \
+                .set_retry(num_retries=2)
+
+
+if __name__ == "__main__":
+    kfp.compiler.Compiler().compile(pipeline, "head-pose-dataset-pipeline.yaml")
