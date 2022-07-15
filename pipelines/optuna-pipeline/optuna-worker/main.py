6c6255c5a21da27a67866f049d1f76017b86206d
tonouchi510 <tonouchi27@gmail.com>
2022-07-15 16:36:43 +0900

    Add simple-training-pipeline to optuna

--- pipelines/optuna-pipeline/optuna-worker/main.py
+++ pipelines/optuna-pipeline/optuna-worker/main.py
@@ -94,7 +94,31 @@ def create_settings(trial):
     """
     settings_file = f"{FLAGS.job_id}-{trial.number}.yaml"
 
-    if FLAGS.training_pipeline_name == "head-pose-pipeline":
+
+    if FLAGS.training_pipeline_name == "simple-training-pipeline":
+        image_size = trial.suggest_categorical(
+            "image_size", [64, 96, 112])
+        model_type = trial.suggest_categorical(
+            "model_type", ["resnet", "efficientnet", "vit"])
+        learning_rate = trial.suggest_categorical(
+            "lr", [1e-5, 1e-4, 1e-3, 1e-2])
+        
+        with open(settings_file, "w") as fp:
+            yaml.dump({
+                "arguments": {
+                    "pipeline_name": FLAGS.training_pipeline_name,
+                    "bucket_name": FLAGS.bucket_name,
+                    "model_type": model_type,
+                    "global_batch_size": 1024,
+                    "epochs": FLAGS.epochs,
+                    "lr": learning_rate,
+                    "image_size": image_size,
+                    "num_classes": 100,
+                    "dataset": FLAGS.dataset,
+                }
+            }, fp)
+
+    elif FLAGS.training_pipeline_name == "head-pose-pipeline":
         image_size = trial.suggest_categorical(
             "image_size", [64, 96, 112])
         model_type = trial.suggest_categorical(
