c4b80c71fefc4acf0408a5742de645f6bea45561
tonouchi27 <tonouchi27@gmail.com>
2022-12-28 18:52:04 +0900

    Add hello-world-pipeline2

---
+++ pipelines/hello-world-pipeline2/hello-world-pipeline2.py
@@ -0,0 +1,34 @@
+import kfp
+from kfp.v2 import dsl
+from kfp import gcp
+
+PIPELINE_NAME = "hello-world-pipeline"
+
+# Initialize component store
+component_store = kfp.components.ComponentStore(
+    local_search_paths=["pipelines/hello-world-pipeline2", "components"])
+
+# Create component factories
+hello_op = component_store.load_component("hello")
+
+
+# Define pipeline
+@dsl.pipeline(
+    name="hello-world-pipeline",
+    description="Output messages"
+)
+def pipeline(
+    job_id: str = "xxxx",
+    message: str = "hello world",
+):
+    hello_op(
+        message=message,
+    ).apply(gcp.use_preemptible_nodepool()) \
+        .set_retry(num_retries=2)
+
+
+if __name__ == "__main__":
+    kfp.v2.compiler.Compiler().compile(
+        pipeline_func=pipeline, 
+        package_path="hello-world-pipeline.json"
+    )
---
+++ pipelines/hello-world-pipeline2/hello/main.py
@@ -0,0 +1,24 @@
+from absl import app
+from absl import flags
+from logging import getLogger
+
+logger = getLogger(__name__)
+
+FLAGS = flags.FLAGS
+flags.DEFINE_string(
+    "message", "hello world",
+    "Message string for output console.")
+
+
+def main(argv):
+    if len(argv) > 1:
+        raise app.UsageError("Too many command-line arguments.")
+    
+    if FLAGS.message:
+        logger.info(FLAGS.message)
+    else:
+        logger.error("No massage error.")
+        exit(-1)
+
+if __name__ == "__main__":
+    app.run(main)
